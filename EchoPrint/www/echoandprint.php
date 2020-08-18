aa3f42d18e55d846585df771143fd10e64188a3e
Igor Torgaiev <Itorgaiev@gmail.com>
2020-08-18 21:50:59 +0200

    echo and print

---
+++ EchoPrint/www/echoandprint.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- <?php
+echo "<h2>PHP is pretty easy and at the same time nice language</h2>";
+echo "<h2>Hello, everyone!</h2>";
+echo "<h2>Guys, can you accept me to study PHP together with you?</h2>";
+echo "Why not? Let's have fun together!<br>";
+echo "This ", "sentence ", "is ", " written ", "normally ", "with ", "the ", "equal ", "space ", "between ", "words.";
+?>  -->
+  
+<!-- <?php
+
+$var1 = "programmer";
+$var2 = "work hard";
+$x = 6;
+
+
+echo "<h2> My advice I want to give you is to be a " .$var1. "</h2>";
+echo "<h2> For that you have got to " .$var2. "</h2>";
+echo "<h2>For that you will need to spend only " .$x. " months</h2>";
+
+?> -->
+
+<!-- PHP Print Statement -->
+
+<!-- <?php
+print "<h2>I love PHP</h2>";
+print "I also love PHP<br>";
+print "So, it's time I started my studies";
+?> -->
+
+<?php
+$var1 = "question";
+$var2 = "ask";
+$var3 = "old";
+$z = 28;
+
+print "<h2> I have a " .$var1. "</h2>";
+print "<h2> You can freely " .$var2. " me any " .$var1. " you wish</h2>";
+print "<h2> How " .$var3. " are you?</h2>";
+print $z;
+
+?>
+
+
+
+</body>
+</html>
