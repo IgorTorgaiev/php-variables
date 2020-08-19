f85457b1dcbd3eb7e27b8a4a81eda623e7f3d1af
Igor Torgaiev <Itorgaiev@gmail.com>
2020-08-19 12:28:17 +0200

    data types example

---
+++ DataTypes/www/datatypes.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<!-- PHP String -->
+
+<!-- <?php
+$a = "Hello, my dear friends<br>";
+$b = "Hello, my dear friends<br>";
+
+echo $a;
+echo "<br>";
+echo $b;
+?> -->
+
+<!-- PHP Integer -->
+
+<!-- <?php
+$x = 9876;
+var_dump($x);
+?> -->
+
+<!-- PHP Float -->
+
+<!-- <?php
+$a = 11.599;
+var_dump($a);
+?> -->
+
+<!-- PHP Array -->
+
+<!-- <?php
+$groups = array("Queen", "The Beatles", "AC/DC");
+var_dump($groups);
+?> -->
+
+<!-- PHP Object -->
+
+<!-- <?php
+class Car {
+    function Car() {
+        $this->model = "Land Rover";
+    }
+}
+
+$Range = new Car();
+echo $Range->model;
+?> -->
+
+<!-- PHP NULL Value -->
+
+<!-- <?php
+$x = "Nice to see you all";
+$x = null;
+var_dump($x);
+?> -->
+
+</body>
+</html>
