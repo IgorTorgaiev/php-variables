223b7a259b2c85a6eb46c03416ad0cdee895a53c
Igor Torgaiev <Itorgaiev@gmail.com>
2020-08-19 13:15:52 +0200

    strings example

---
+++ Strings/www/strings.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- String Functions -->
+<!-- Return a length of a string : strlen()-->
+<!-- <?php
+echo strlen("Hello! How is everything going, man?!");
+?> -->
+
+<!-- Now, let's learn how to count words in a string: str_word_count() -->
+
+<!-- <?php
+echo str_word_count("How many words are here? Just count, please");
+?> -->
+
+<!-- Do you know how to reverse a string? : strrev() -->
+
+<!-- <?php
+echo strrev("Wanna know what is written here?");
+echo "<br><br>";
+echo strrev("?ereh nettirw si tahw wonk annaW");
+?> -->
+
+<!-- Now let's search for a text in a string : strpos()-->
+
+<!-- <?php
+echo strpos("Who are you?", "you");
+?> -->
+
+<!-- What about the text replacement inside the string?: str_replace() -->
+
+<?php
+echo str_replace("Gary", "Igor", "Just call me Gary!");
+?>
+
+
+</body>
+</html>
